936ce57c1eb6c0c61722a2b1fb829bd6675795be
frjo_hendrix <frjo@di.ku.dk>
2024-01-24 14:18:56 +0100

    refactoring and split, delete of Code

--- Code/graphgym/custom_graphgym/loader/NGS_loader.py
+++
@@ -1,9 +0,0 @@
-from Code.datasetClass import InOrgMatDatasets
-from torch_geometric.graphgym.register import register_loader
-
-
-@register_loader('NGS-dataset')
-def load_NGS_dataset(format, name, dataset_dir):
-    if format == 'PyG':
-        dataset_raw = InOrgMatDatasets(dataset=name, root=dataset_dir)
-        return dataset_raw
