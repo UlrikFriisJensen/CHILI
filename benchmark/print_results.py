63527ef0d6a80a09dd5e6f49391dcfab731a8e64
UlrikFriis-Jensen <lgb543@alumni.ku.dk>
2024-02-06 15:17:06 -0500

    Updated print_results.py

--- benchmark/print_results.py
+++ benchmark/print_results.py
@@ -1,9 +1,18 @@
-##! Imports
+#%% Imports
 import os
 import pandas as pd
+import numpy as np
+import warnings 
+warnings.simplefilter(action='ignore')
 
-##! Main    
-def main(results_dir, fstring_format='.4f'):
+#%% Functions
+def custom_aggfunc(x, fstring_format='.3f'):
+    if x.std() > 0.00001:
+        return rf'${x.mean():{fstring_format}} \pm {x.std():{fstring_format}}$'
+    else:
+        return rf'${x.mean():{fstring_format}}$'
+   
+def main(results_dir, fstring_format='.3f'):
     # Initialize an empty list to store the dataframes
     dfs = []
 
@@ -28,36 +37,58 @@ def main(results_dir, fstring_format='.4f'):
     combined_df['Val F1-score'] = combined_df['Val F1-score'].str.split('(').str[1].str.split(')').str[0].astype(float)
     combined_df['Test F1-score'] = combined_df['Test F1-score'].str.split('(').str[1].str.split(')').str[0].astype(float)
 
-    # Print the results
-    for task in combined_df['Task'].unique():
-        print('')
-        print('-'*50)
-        print('')
-        print(task)
-        for model in combined_df['Model'].unique():
-            print('')
-            print(model)
-            val_mae_mean = combined_df[(combined_df['Task'] == task) & (combined_df['Model'] == model)]['Val posMAE/MSE'].mean()
-            val_mae_std = combined_df[(combined_df['Task'] == task) & (combined_df['Model'] == model)]['Val posMAE/MSE'].std()
-            test_mae_mean = combined_df[(combined_df['Task'] == task) & (combined_df['Model'] == model)]['Test posMAE/MSE'].mean()
-            test_mae_std = combined_df[(combined_df['Task'] == task) & (combined_df['Model'] == model)]['Test posMAE/MSE'].std()
-            
-            print(f'Val posMAE/MSE: ${val_mae_mean:{fstring_format}} \pm {val_mae_std:{fstring_format}}$')
-            print(f'Test posMAE/MSE: ${test_mae_mean:{fstring_format}} \pm {test_mae_std:{fstring_format}}$')
-            
-            val_f1_mean = combined_df[(combined_df['Task'] == task) & (combined_df['Model'] == model)]['Val F1-score'].mean()
-            val_f1_std = combined_df[(combined_df['Task'] == task) & (combined_df['Model'] == model)]['Val F1-score'].std()
-            test_f1_mean = combined_df[(combined_df['Task'] == task) & (combined_df['Model'] == model)]['Test F1-score'].mean()
-            test_f1_std = combined_df[(combined_df['Task'] == task) & (combined_df['Model'] == model)]['Test F1-score'].std()
-            
-            print(f'Val F1-score: ${val_f1_mean:{fstring_format}} \pm {val_f1_std:{fstring_format}}$')
-            print(f'Test F1-score: ${test_f1_mean:{fstring_format}} \pm {test_f1_std:{fstring_format}}$')
+    # Add a new column to the dataframe that merges the relevant test metrics
+    combined_df['Test metric'] = [combined_df['Test posMAE/MSE'][i] if np.isnan(combined_df['Test F1-score'][i]) else combined_df['Test F1-score'][i] for i in range(len(combined_df))]
+    
+    # Split the dataframe into two separate dataframes
+    property_df = combined_df[~combined_df['Task'].str.endswith(('SAXS', 'xPDF', 'XRD'))]
+    structure_df = combined_df[combined_df['Task'].str.endswith(('SAXS', 'xPDF', 'XRD'))]
+    
+    ### Property dataframe
+    # Convert the 'Dataset' and 'Model' columns to categorical data types
+    property_df['Dataset'] = property_df['Dataset'].astype(pd.api.types.CategoricalDtype(categories=['CHILI-SIM', 'CHILI-COD']))
+    property_df['Model'] = property_df['Model'].astype(pd.api.types.CategoricalDtype(categories=['RandomClass', 'MostFrequentClass', 'Mean', 'GCN', 'GraphSAGE', 'GAT', 'GIN', 'EdgeCNN', 'GraphUNet', 'PMLP']))
+    
+    # Specify the order of the rows and columns
+    column_order = ['AtomClassification', 'CrystalSystemClassification', 'SpacegroupClassification', 'PositionRegression', 'DistanceRegression', 'SAXSRegression', 'XRDRegression', 'xPDFRegression']
+    # Row order from least to most trainable parameters
+    # row_order = ['RandomClass', 'MostFrequentClass', 'Mean', 'GCN', 'PMLP', 'GraphSAGE', 'GAT', 'GraphUNet', 'GIN', 'EdgeCNN']
+    
+    # Create and save the LaTeX table
+    property_df.pivot_table(index=['Dataset', 'Model'], columns='Task', values='Test metric', aggfunc=custom_aggfunc).reindex(column_order, axis=1).to_latex(f'./{results_dir}/propertyPredictionTasksTable.tex', column_format='lccccccccc')
+    
+    ### Structure dataframe
+    # Convert the 'Dataset' column to categorical data types
+    structure_df['Dataset'] = structure_df['Dataset'].astype(pd.api.types.CategoricalDtype(categories=['CHILI-SIM', 'CHILI-COD']))
+    
+    # Specify the order of the columns
+    column_order = [
+        'CrystalSystemClassificationSAXS',
+        'CrystalSystemClassificationXRD',
+        'CrystalSystemClassificationxPDF',
+        'SpacegroupClassificationSAXS',
+        'SpacegroupClassificationXRD',
+        'SpacegroupClassificationxPDF',
+        'CellParamsRegressionSAXS',
+        'CellParamsRegressionXRD',
+        'CellParamsRegressionxPDF',
+        'UnitCellPositionRegressionSAXS',
+        'UnitCellPositionRegressionXRD',
+        'UnitCellPositionRegressionxPDF',
+        'AbsPositionRegressionSAXS',
+        'AbsPositionRegressionXRD',
+        'AbsPositionRegressionxPDF',
+    ]
+    
+    # Create and save the LaTeX table
+    structure_df.pivot_table(index=['Task'], columns='Dataset', values='Test metric', aggfunc=custom_aggfunc).reindex(column_order, axis=0).to_latex(f'./{results_dir}/structurePredictionTasksTable.tex', column_format='lcc')
 
+#%% Main
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--results_dir', required=True, type=str)
-    parser.add_argument('--fstring_format', default='.4f', type=str)
+    parser.add_argument('--fstring_format', default='.3f', type=str)
     args = vars(parser.parse_args())
 
     main(args['results_dir'], args['fstring_format'])
